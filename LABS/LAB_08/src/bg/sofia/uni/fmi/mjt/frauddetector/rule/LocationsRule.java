94ecc5694861878b00379515cdf919d67ad6ed2d
Йоана Матева <107748308+ymatevva@users.noreply.github.com>
2025-08-13 16:49:44 +0300

    Changing logic of a method.

--- LABS/LAB_08/src/bg/sofia/uni/fmi/mjt/frauddetector/rule/LocationsRule.java
+++ LABS/LAB_08/src/bg/sofia/uni/fmi/mjt/frauddetector/rule/LocationsRule.java
@@ -12,13 +12,11 @@ public class LocationsRule extends RuleImpl {
 
     // if the operations are made from more than 'threshold' locations => true
 
-    @Override
+     @Override
     public boolean applicable(List<Transaction> transactions) {
-        int countOp = (int)transactions.stream()
-            .map(Transaction::location)
-            .distinct()
-            .count();
 
-        return countOp > super.threshold();
+        return transactions.stream()
+            .collect(Collectors.groupingBy(Transaction::location
+                ,Collectors.counting())).size() > threshold();
     }
 }
