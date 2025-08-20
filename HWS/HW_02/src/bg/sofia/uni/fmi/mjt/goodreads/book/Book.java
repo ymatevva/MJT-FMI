595c234d6bc39d7283ff74403beed854eed9eb0d
ymatevva <yoanamatevva@gmail.com>
2025-08-20 20:31:51 +0300

    Add Book not completed implementation.

--- HWS/HW_02/src/bg/sofia/uni/fmi/mjt/goodreads/book/Book.java
+++ HWS/HW_02/src/bg/sofia/uni/fmi/mjt/goodreads/book/Book.java
@@ -1,4 +1,20 @@
 package bg.sofia.uni.fmi.mjt.goodreads.book;
 
-public class Book {
+import java.util.List;
+
+public record Book(
+    String ID,
+    String title,
+    String author,
+    String description,
+    List<String> genres,
+    double rating,
+    int ratingCount,
+    String URL
+) {
+
+    public static Book of(String[] tokens) {
+        throw new UnsupportedOperationException("Not yet implemented");
+
+    }
 }
