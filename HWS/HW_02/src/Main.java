f8f0bd480e9195c1c2b1e8ea4e924a2538bcb7ba
ymatevva <yoanamatevva@gmail.com>
2025-08-23 23:22:32 +0300

    Manual testing of BookFinder functionality.

--- HWS/HW_02/src/Main.java
+++ HWS/HW_02/src/Main.java
@@ -1,11 +1,13 @@
 import bg.sofia.uni.fmi.mjt.goodreads.BookLoader;
 import bg.sofia.uni.fmi.mjt.goodreads.book.Book;
 import bg.sofia.uni.fmi.mjt.goodreads.finder.BookFinder;
+import bg.sofia.uni.fmi.mjt.goodreads.finder.MatchOption;
 import bg.sofia.uni.fmi.mjt.goodreads.tokenizer.TextTokenizer;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.Reader;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -24,11 +26,23 @@ public class Main {
         BookFinder bookFinder = new BookFinder(books, textTokenizer);
 
         System.out.println("Find by author Agata Christie");
-        List<Book> foundByAuthorList = bookFinder.searchByAuthor("Agata Christie");
+        List<Book> foundByAuthorList = bookFinder.searchByAuthor("Agatha Christie");
 
-        for (var book : books) {
+        for (var book : foundByAuthorList) {
             System.out.println(book.toString());
         }
 
+        List<Book> foundByKeyword = bookFinder.searchByKeywords(Set.of("pink"), MatchOption.MATCH_ALL);
+
+        for (var book : foundByKeyword) {
+            System.out.println(book.title());
+        }
+
+        List<Book> foundByGenres = bookFinder.searchByGenres(Set.of("Romance", "Magic"),MatchOption.MATCH_ALL);
+
+        for (var book : foundByGenres) {
+            System.out.println(book.title());
+        }
     }
+
 }
