69a2cb7260b933064539dec521b788594415e3fc
ymatevva <yoanamatevva@gmail.com>
2025-08-27 17:06:51 +0300

    Add some updates on implementation on analyzer.

--- LABS/LAB_09/src/bg/sofia/uni/fmi/mjt/sentimentanalyzer/ParallelSentimentAnalyzer.java
+++ LABS/LAB_09/src/bg/sofia/uni/fmi/mjt/sentimentanalyzer/ParallelSentimentAnalyzer.java
@@ -3,24 +3,38 @@ package bg.sofia.uni.fmi.mjt.sentimentanalyzer;
 import bg.sofia.uni.fmi.mjt.sentimentanalyzer.AnalyzerInput;
 import bg.sofia.uni.fmi.mjt.sentimentanalyzer.SentimentAnalyzerAPI;
 import bg.sofia.uni.fmi.mjt.sentimentanalyzer.SentimentScore;
+
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.Reader;
 
 import bg.sofia.uni.fmi.mjt.sentimentanalyzer.exceptions.SentimentAnalysisException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 
 public class ParallelSentimentAnalyzer implements SentimentAnalyzerAPI {
 
+    private int workersCount;
+    private Set<String> stopWords;
+    private Map<String, SentimentScore> sentimentLexicon;
+    private final String PUNCTUAL_REGEX = "\\p{Punct}";
+    private final String SPACE_REGEX = "\\s+";
 
 
-    /**
-     * @param workersCount     number of consumer workers
-     * @param stopWords        set containing stop words
-     * @param sentimentLexicon map containing the sentiment lexicon, where the key is the word and the value is the sentiment score
-     */
     public ParallelSentimentAnalyzer(int workersCount, Set<String> stopWords,
                                      Map<String, SentimentScore> sentimentLexicon) {
+
+        this.sentimentLexicon = sentimentLexicon;
+        this.stopWords = stopWords;
+        this.workersCount = workersCount;
     }
 
 
@@ -40,24 +54,68 @@ public class ParallelSentimentAnalyzer implements SentimentAnalyzerAPI {
      *
      *                                    <p><b>Example:</b></p>
      *                                    <pre>
-     *                                                                       {@code
-     *                                                                       AnalyzerInput input1 = new AnalyzerInput("doc1", new StringReader("I love java"));
-     *                                                                       AnalyzerInput input2 = new AnalyzerInput("doc2", new StringReader("I hate bugs"));
+     *                                                                                                                                                                                {@code
+     *                                                                                                                                                                                AnalyzerInput input1 = new AnalyzerInput("doc1", new StringReader("I love java"));
+     *                                                                                                                                                                                AnalyzerInput input2 = new AnalyzerInput("doc2", new StringReader("I hate bugs"));
      *
-     *                                                                       ParallelSentimentAnalyzer analyzer = new ParallelSentimentAnalyzer(4, stopWordsReader, lexiconReader);
-     *                                                                       Map<String, SentimentScore> results = analyzer.analyze(input1, input2);
+     *                                                                                                                                                                                ParallelSentimentAnalyzer analyzer = new ParallelSentimentAnalyzer(4, stopWordsReader, lexiconReader);
+     *                                                                                                                                                                                Map<String, SentimentScore> results = analyzer.analyze(input1, input2);
      *
-     *                                                                       // Example output:
-     *                                                                       // {
-     *                                                                       //   "doc1": MODERATELY_POSITIVE,
-     *                                                                       //   "doc2": SLIGHTLY_NEGATIVE
-     *                                                                       // }
-     *                                                                       }
-     *                                                                       </pre>
+     *                                                                                                                                                                                // Example output:
+     *                                                                                                                                                                                // {
+     *                                                                                                                                                                                //   "doc1": MODERATELY_POSITIVE,
+     *                                                                                                                                                                                //   "doc2": SLIGHTLY_NEGATIVE
+     *                                                                                                                                                                                // }
+     *                                                                                                                                                                                }
+     *                                                                                                                                                                                </pre>
      */
 
     @Override
     public Map<String, SentimentScore> analyze(AnalyzerInput... input) {
+
+        Thread[] threads = new Thread[input.length];
+
+        for (int i = 0; i < input.length; i++) {
+            threads = new Thread[] {new Thread()};
+        }
+
+
+        Map<String, SentimentScore> currInput = analyzeInput(input[0]);
+
         return Map.of();
+    }
+
+    // SHOULDNT BE HERE AT ALL
+    // TO BE UPDATED
+    private Map<String, SentimentScore> analyzeInput(AnalyzerInput input) {
+        StringBuilder textInput = new StringBuilder();
+
+        try (BufferedReader bufferedReader = new BufferedReader(input.inputReader())) {
+
+            String currLine;
+            while ((currLine = bufferedReader.readLine()) != null) {
+                textInput.append(currLine + System.lineSeparator());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error while reading.");
+        }
+
+
+        String text = textInput.toString();
+        text = text.toLowerCase();
+        text = text.replaceAll(PUNCTUAL_REGEX, "");
+        text = text.replaceAll(SPACE_REGEX, " ");
+        List<String> words = Arrays.stream(text.split(" "))
+            .filter(word -> !stopWords.contains(word))
+            .toList();
+
+        Map<String, SentimentScore> scores = words.stream()
+            .collect(Collectors.toMap(
+                word -> word,
+                word -> sentimentLexicon.get(word),
+                (o, n) -> o,
+                HashMap::new
+            ));
+        return scores;
     }
 }
--- LABS/LAB_09/test/bg/sofia/uni/fmi/mjt/sentimentanalyzer/ParallelSentimentAnalyzerTest.java
+++ LABS/LAB_09/test/bg/sofia/uni/fmi/mjt/sentimentanalyzer/ParallelSentimentAnalyzerTest.java
@@ -1,4 +1,6 @@
 package bg.sofia.uni.fmi.mjt.sentimentanalyzer;
 
 public class ParallelSentimentAnalyzerTest {
+
+
 }
