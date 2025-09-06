b4274aeb25e7b992e8a32de1e11b64433c64f884
Йоана Матева <107748308+ymatevva@users.noreply.github.com>
2025-09-06 14:23:14 +0300

    Update ParallelSentimentAnalyzerTest.java

--- LABS/LAB_09/test/bg/sofia/uni/fmi/mjt/sentimentanalyzer/ParallelSentimentAnalyzerTest.java
+++ LABS/LAB_09/test/bg/sofia/uni/fmi/mjt/sentimentanalyzer/ParallelSentimentAnalyzerTest.java
@@ -1,6 +1,41 @@
-package bg.sofia.uni.fmi.mjt.sentimentanalyzer;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 public class ParallelSentimentAnalyzerTest {
 
+    private Set<String> stopwords = Set.of("a", "of", "and", "or", "if");
+    private int workersCount = 4;
+    private Map<String, SentimentScore> scores = Map.of(
+        "love", SentimentScore.MODERATELY_POSITIVE,
+        "sad", SentimentScore.MODERATELY_NEGATIVE,
+        "cat", SentimentScore.NEUTRAL,
+        "super", SentimentScore.POSITIVE,
+        "tired", SentimentScore.SLIGHTLY_NEGATIVE,
+        "awful", SentimentScore.NEGATIVE,
+        "perfect", SentimentScore.VERY_POSITIVE);
 
+
+
+    @Test
+    void testAnalyzeSentiment() {
+        ParallelSentimentAnalyzer analyzer = new ParallelSentimentAnalyzer(workersCount, stopwords, scores);
+        AnalyzerInput input1 = Mockito.mock(AnalyzerInput.class);
+        AnalyzerInput input2 = Mockito.mock(AnalyzerInput.class);
+
+        when(input1.inputID()).thenReturn("doc1");
+        when(input2.inputID()).thenReturn("doc2");
+
+        when(input1.inputReader()).thenReturn(new StringReader("If she is tired the day is awful and sad."));
+        when(input2.inputReader()).thenReturn(new StringReader("The cats are super tired and love to sleep."));
+
+        Map<String, SentimentScore> analyzedDocs = analyzer.analyze(input1, input2);
+
+        assertEquals(2, analyzedDocs.size(), "Method should return the same count of pairs as the input size");
+        assertEquals(SentimentScore.VERY_NEGATIVE, analyzedDocs.get(input1.inputID()),
+            "Method should correctly calculate the sentiment score.");
+        assertEquals(SentimentScore.VERY_POSITIVE, analyzedDocs.get(input2.inputID()),
+            "Method should correctly calculate the sentiment score.");
+    }
 }
