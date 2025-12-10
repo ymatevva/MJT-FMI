cc4f2e28ecfe1790f0cae142073d09cb91a3a6ef
Йоана Матева <107748308+ymatevva@users.noreply.github.com>
2025-12-10 09:44:29 +0200

    Add files via upload

---
+++ LABS/LAB_07/src/bg/sofia/uni/fmi/mjt/imagekit/algorithm/detection/EdgeDetectionAlgorithm.java
@@ -0,0 +1,7 @@
+package bg.sofia.uni.fmi.mjt.imagekit.algorithm.detection;
+
+import bg.sofia.uni.fmi.mjt.imagekit.algorithm.ImageAlgorithm;
+
+public interface EdgeDetectionAlgorithm extends ImageAlgorithm {
+
+}
