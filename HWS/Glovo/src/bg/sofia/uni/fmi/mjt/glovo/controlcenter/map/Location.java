64fec4a8c9d58ca6f3bbc238f4234693233f709c
Йоана Матева <107748308+ymatevva@users.noreply.github.com>
2026-02-06 18:12:37 +0200

    Delete directory

--- HWS/Glovo/src/bg/sofia/uni/fmi/mjt/glovo/controlcenter/map/Location.java
+++
@@ -1,4 +0,0 @@
-package bg.sofia.uni.fmi.mjt.glovo.controlcenter.map;
-
-public record Location(int x, int y) {
-}
--- HWS/Goodreads/test/bg/sofia/uni/fmi/mjt/goodreads/TestBookLoader.java
+++
@@ -1,4 +0,0 @@
-package bg.sofia.uni.fmi.mjt.goodreads;
-
-public class TestBookLoader {
-}
